ee6b45f23c39305e8fac07719f87c16e3fc1f579
dlhardi <derricklhardison@gmail.com>
2021-04-19 13:59:35 -0400

    Selected some of the needed elements. Set starting conditions for game. Added event listeners to some buttons.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+
+// Selecting elements
+const score0EL = document.querySelector("#score--0");
+const score1EL = document.querySelector("#score--1");
+const newGameBTN = document.querySelector(".btn--new");
+const rollDiceBTN = document.querySelector(".btn--roll");
+const diceEL = document.querySelector(".dice");
+
+// Starting conditions
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add("hidden");
+
+// Event listeners
+newGameBTN.addEventListener("click", function () {
+  console.log("I was clicked");
+  // score0EL.textContent = 0;
+  // score1EL.textContent = 0;
+  // diceEL.classList = "hidden";
+});
+
+rollDiceBTN.addEventListener("click", function () {
+  diceEL.classList.remove("hidden");
+  console.log(rollDiceBTN);
+});
+
+console.log(score0EL);
+console.log(score1EL);
+console.log(diceEL);
+console.log(rollDiceBTN);
